05587cb57d10458a297c0de063fe11e40c158db0
rishit-exe <rishit.vns05@gmail.com>
2026-02-20 09:29:18 +0530

    UC1: Add welcome message module

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,5 +1,7 @@
 public class PalindromeCheckerApp {
     static void main() {
-        System.out.println("Hello world");
+        System.out.println("Welcome to the Palindrome Checker Management System");
+        System.out.println("Version : 1.0");
+        System.out.println("System initialized successfully.");
     }
 }
